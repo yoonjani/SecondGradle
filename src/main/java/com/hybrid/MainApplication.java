9f8b8e3b978a546b88990c6dd907fe5b49a2116f
WorkStation <WorkStation>
2016-07-19 20:15:25 +0900

    10ea Hello

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -4,8 +4,10 @@ public class MainApplication {
 
 	public static void main(String[] args) {
 		System.out.println("###################");
+		for(int i=0; i<10; i++) {
 		System.out.println("Hello Second Gradle");
 		System.out.println("###################");
+		}
 	}
 
 }
